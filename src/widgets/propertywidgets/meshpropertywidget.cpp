c649ffbe7f21b35a9577efdbdd44e239816289fc
Nicolas Brown <nclsbrwn@gmail.com>
2017-03-16 13:55:10 -0500

    Added Jahshaka license to files in the jahshaka project

--- src/widgets/propertywidgets/meshpropertywidget.cpp
+++ src/widgets/propertywidgets/meshpropertywidget.cpp
@@ -1,3 +1,14 @@
+/**************************************************************************
+This file is part of JahshakaVR, VR Authoring Toolkit
+http://www.jahshaka.com
+Copyright (c) 2016  GPLv3 Jahshaka LLC <coders@jahshaka.com>
+
+This is free software: you may copy, redistribute
+and/or modify it under the terms of the GPLv3 License
+
+For more information see the LICENSE file
+*************************************************************************/
+
 #include "meshpropertywidget.h"
 #include "../filepickerwidget.h"
 #include "../../irisgl/src/scenegraph/meshnode.h"
