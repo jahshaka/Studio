f6e88e72d0b02a7cfd20e0f29d6eb1adb9d7455a
Nicolas Brown <nclsbrwn@gmail.com>
2017-04-20 17:42:13 -0500

    Added radial blur post process

--- src/irisgl/src/postprocesses/radialblurpostprocess.cpp
+++ src/irisgl/src/postprocesses/radialblurpostprocess.cpp
@@ -6,6 +6,7 @@
 #include "../graphics/postprocessmanager.h"
 #include "../graphics/postprocess.h"
 #include "../graphics/graphicshelper.h"
+#include "../materials/propertytype.h"
 
 namespace iris
 {
@@ -14,14 +15,45 @@ RadialBlurPostProcess::RadialBlurPostProcess()
 {
     shader = GraphicsHelper::loadShader(":assets/shaders/postprocesses/default.vs",
                                         ":assets/shaders/postprocesses/radial_blur.fs");
+    blurSize = 1.0f;
 }
 
 void RadialBlurPostProcess::process(PostProcessContext *ctx)
 {
     shader->bind();
     shader->setUniformValue("u_sceneTexture", 0);
+    shader->setUniformValue("u_blurSize", blurSize);
     ctx->manager->blit(ctx->sceneTexture, ctx->finalTexture, shader);
+
     shader->release();
+}
+
+QList<Property *> RadialBlurPostProcess::getProperties()
+{
+    auto props = QList<Property*>();
+
+    auto prop = new FloatProperty();
+    prop->displayName = "Blur Scale";
+    prop->name = "blur_scale";
+    prop->value = 1.0f;
+    prop->minValue = 0.1f;
+    prop->maxValue = 10.0f;
+    //prop->id = 1;
+    props.append(prop);
+
+    return props;
+}
+
+void RadialBlurPostProcess::setProperty(Property *prop)
+{
+    if(prop->name == "blur_scale") {
+        blurSize = ((FloatProperty*)prop)->value;
+    }
+}
+
+RadialBlurPostProcessPtr RadialBlurPostProcess::create()
+{
+    return RadialBlurPostProcessPtr(new RadialBlurPostProcess());
 }
 
 }
--- src/irisgl/src/postprocesses/radialblurpostprocess.h
+++ src/irisgl/src/postprocesses/radialblurpostprocess.h
@@ -9,13 +9,23 @@ class QOpenGLShaderProgram;
 namespace iris
 {
 
+class RadialBlurPostProcess;
+typedef QSharedPointer<RadialBlurPostProcess> RadialBlurPostProcessPtr;
+
 class RadialBlurPostProcess : public PostProcess
 {
     QOpenGLShaderProgram* shader;
+
+    float blurSize;
 public:
     RadialBlurPostProcess();
 
     virtual void process(PostProcessContext* ctx) override;
+
+    virtual QList<Property*> getProperties() override;
+    virtual void setProperty(Property* prop) override;
+
+    static RadialBlurPostProcessPtr create();
 };
 
 }
