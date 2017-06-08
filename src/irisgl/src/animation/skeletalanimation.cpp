00cb2d12c89bde315ccedc8002998b52bac7d201
Nicolas Brown <nclsbrwn@gmail.com>
2017-06-08 12:36:46 -0500

    Correct animation length is calculated and looping now enabled

--- src/irisgl/src/animation/skeletalanimation.cpp
+++ src/irisgl/src/animation/skeletalanimation.cpp
@@ -1,11 +1,20 @@
 #include "skeletalanimation.h"
 #include "SkeletonMeshBuilder.h"
+#include "../animation/keyframeanimation.h"
 
 namespace iris {
 
 void SkeletalAnimation::addBoneAnimation(QString boneName, iris::BoneAnimation *boneAnim)
 {
     boneAnimations.insert(boneName, QSharedPointer<BoneAnimation>(boneAnim));
+}
+
+float BoneAnimation::getLength()
+{
+    float maxLength = 0;
+    maxLength = posKeys->getLength();
+    maxLength = qMax(maxLength, rotKeys->getLength());
+    return qMax(maxLength, scaleKeys->getLength());
 }
 
 }
--- src/irisgl/src/core/scenenode.cpp
+++ src/irisgl/src/core/scenenode.cpp
@@ -261,6 +261,8 @@ void SceneNode::updateAnimation(float time)
     */
 
     if (!!animation) {
+
+        time = animation->getSampleTime(time);
         if (animation->hasPropertyAnim("position")) {
             pos = animation->getVector3PropertyAnim("position")->getValue(time);
         }
