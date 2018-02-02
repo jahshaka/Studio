005ad807fa4e50a36b8e8dabc36c38107a362033
Nicolas Brown <nclsbrwn@gmail.com>
2018-02-02 14:42:29 -0500

    VR view in viewport now works properly in 4k screens

--- irisgl/src/graphics/forwardrenderer.cpp
+++ irisgl/src/graphics/forwardrenderer.cpp
@@ -501,7 +501,7 @@ void ForwardRenderer::renderSceneVr(float delta, Viewport* vp, bool useViewer)
    //rendering to the window
    gl->glBindFramebuffer(GL_FRAMEBUFFER, ctx->defaultFramebufferObject());
 
-   gl->glViewport(0, 0, vp->width,vp->height);
+   gl->glViewport(0, 0, vp->width * vp->pixelRatioScale,vp->height * vp->pixelRatioScale);
    gl->glActiveTexture(GL_TEXTURE0);
 
    graphics->setBlendState(BlendState::Opaque);
