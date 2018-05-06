e745d60d69be7010f26dd66e757be01927a7c08a
Nicolas Brown <nclsbrwn@gmail.com>
2018-05-06 17:57:34 -0500

    Switched to https version of asset url

--- src/widgets/assetview.cpp
+++ src/widgets/assetview.cpp
@@ -596,7 +596,7 @@ AssetView::AssetView(Database *handle, QWidget *parent) : db(handle), QWidget(pa
 	QPushButton *downloadWorld = new QPushButton("Download Assets");
 
 	connect(downloadWorld, &QPushButton::pressed, []() {
-		QDesktopServices::openUrl(QUrl("http://www.jahfx.com/downloads/models/"));
+		QDesktopServices::openUrl(QUrl("https://www.jahfx.com/downloads/models/"));
 	});
 
 	QWidget *importButtons = new QWidget;
