e4784afe0af5b57dc9cc3405500534e7527f15cf
Unknown <willroymyles@yahoo.com>
2018-05-24 14:14:15 -0500

    Added updated miner

--- src/misc/minerui.cpp
+++ src/misc/minerui.cpp
@@ -1,16 +1,20 @@
 
 #include "minerui.h"
+#include <QComboBox>
 #include <QDebug>
 #include <QGraphicsDropShadowEffect>
+#include <QGroupBox>
 
 MinerUI::MinerUI(QWidget *parent)
-    : QWidget(parent)
+	: QWidget(parent)
 {
-    configureUI();
-    configureConnections();
-    //setAttribute(Qt::WA_TranslucentBackground);
-    setWindowFlags(Qt::FramelessWindowHint |Qt::WindowStaysOnTopHint | Qt::X11BypassWindowManagerHint);
-	setWindowFlag(Qt::SubWindow);
+	configureUI();
+	configureSettings();
+	configureConnections();
+	configureStyleSheet();
+	setAttribute(Qt::WA_TranslucentBackground);
+	setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint | Qt::X11BypassWindowManagerHint);
+	// setWindowFlag(Qt::SubWindow);
 	setAttribute(Qt::WA_QuitOnClose, false);
 	setWindowModality(Qt::ApplicationModal);
 }
@@ -22,74 +26,92 @@ MinerUI::~MinerUI()
 
 void MinerUI::addGraphicsCard(QString string)
 {
-    GraphicsCardUI *card = new GraphicsCardUI();
-    card->setCardName(string);
+	GraphicsCardUI *card = new GraphicsCardUI();
+	card->setCardName(string);
 	card->setObjectName(QStringLiteral("card"));
-    list.append(card);
-    cardHolderLayout->addWidget(card);
+	list.append(card);
+	cardHolderLayout->addWidget(card);
 }
 
 void MinerUI::configureUI()
 {
-	
-	fontIcon.initFontAwesome();
-    setFixedWidth(490);
-    setObjectName(QStringLiteral("this"));
-    cardHolderLayout = new QVBoxLayout();
-    auto bottomLayout = new QHBoxLayout();
-    auto autoLayout = new QVBoxLayout();
-    layout = new QVBoxLayout();
-    setLayout(layout);
+	setContentsMargins(20, 20, 20, 20);
 
-    toolbar = new QToolBar();
+	QGraphicsDropShadowEffect *effect = new QGraphicsDropShadowEffect;
+	effect->setBlurRadius(20);
+	effect->setXOffset(0);
+	effect->setYOffset(0);
+	effect->setColor(QColor(0, 0, 0, 200));
+	//setGraphicsEffect(effect);
+
+
+	stack = new QStackedWidget;
+	stack->setGraphicsEffect(effect);
+
+
+	// fontIcon.initFontAwesome();
+
+	auto groupBoxLayout = new QVBoxLayout;
+	auto mainLayout = new QVBoxLayout;
+	auto autoLayout = new QVBoxLayout;
+	auto bottomLayout = new QHBoxLayout;
+	cardHolderLayout = new QVBoxLayout;
+
+	setLayout(mainLayout);
+	mainLayout->addWidget(stack);
+
+	auto groupBox = new QGroupBox;
+	groupBox->setLayout(groupBoxLayout);
+	groupBox->setFixedSize(500, 300);
+	//mainLayout->addWidget(groupBox);
+	stack->addWidget(groupBox);
+
+	//configure toolbar
+
+
+	toolbar = new QToolBar();
 	toolbar->setObjectName(QStringLiteral("toolBar"));
-    settings = new QAction("settings");
-	settings->setText(QChar(fa::cog));
-	settings->setFont(fontIcon.font(15));
-    advance = new QAction("advance");
-	advance->setText(QChar(fa::sliders));
-	advance->setFont(fontIcon.font(15));
-    close = new QAction("X");
-	close->setText(QChar(fa::times));
-	close->setFont(fontIcon.font(15));
+	settings = new QAction("settings");
+	//settings->setText(QChar(fa::cog));
+	//settings->setFont(fontIcon.font(15));
+	advance = new QAction("advance");
+	//advance->setText(QChar(fa::sliders));
+	//advance->setFont(fontIcon.font(15));
+	close = new QAction("X");
+	//close->setText(QChar(fa::times));
+	//close->setFont(fontIcon.font(15));
 
-    toolbar->addAction(settings);
-    toolbar->addAction(advance);
-    auto empty = new QWidget();
-    empty->setSizePolicy(QSizePolicy::Expanding,QSizePolicy::Preferred);
-    //toolbar->addSeparator();
-    toolbar->addWidget(empty);
-    toolbar->addAction(close);
-
+	toolbar->addAction(settings);
+	toolbar->addAction(advance);
+	auto empty = new QWidget();
+	empty->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred);
+	toolbar->addWidget(empty);
+	toolbar->addAction(close);
 
 
-    scrollHolder= new QScrollArea(this);
-	scrollHolder->setContentsMargins(0, 3, 0, 3);
-    scrollHolder->setAlignment(Qt::AlignTop);
-	scrollHolder->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-
-    cardHolder = new QWidget();
-    cardHolder->setObjectName(QStringLiteral("cardHolder"));
-    cardHolder->setGeometry(0,0,450,400);
+	scrollArea = new QScrollArea(this);
+	scrollArea->setContentsMargins(0, 3, 3, 3);
+	scrollArea->setAlignment(Qt::AlignTop);
+	scrollArea->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 
 
-    layout->addWidget(toolbar);
-	layout->addSpacing(3);
-    layout->addWidget(scrollHolder);
+	cardHolder = new QWidget();
+	cardHolder->setObjectName(QStringLiteral("cardHolder"));
+	cardHolder->setGeometry(0, 0, 450, 400);
 
-    for(int i=0;i< 2; i++){
-        addGraphicsCard("amd "+QString::number(i));
-    }
+	for (int i = 0; i< 2; i++) {
+		addGraphicsCard("amd numbering at this pos " + QString::number(i));
+	}
 
-    cardHolder->setLayout(cardHolderLayout);
-    cardHolderLayout->setSizeConstraint(QLayout::SetFixedSize);
+	cardHolder->setLayout(cardHolderLayout);
+	cardHolderLayout->setSizeConstraint(QLayout::SetFixedSize);
 	cardHolderLayout->setContentsMargins(0, 0, 0, 0);
 	cardHolderLayout->setSpacing(2);
-	scrollHolder->setWidget(cardHolder);
+	scrollArea->setWidget(cardHolder);
 
-    startBtn = new QPushButton("Start", this);
-    coinType = new QLabel("JFX: 0.00123");
-    autostart = new QLabel("auto start");
+	startBtn = new QPushButton("Start", this);
+	coinType = new QLabel("JFX: 0.00123");
+	autostart = new QLabel("auto start");
 	autostart->setAlignment(Qt::AlignHCenter);
 	startBtn->setObjectName(QStringLiteral("startBtn"));
 
@@ -100,80 +122,205 @@ void MinerUI::configureUI()
 	//autostart->setFont(font);
 
 
-    autoStartSwitch = new MSwitch();
-    autoStartSwitch->setColor(QColor(40,128,185));
-    autoStartSwitch->setSizeOfSwitch(20);
+	autoStartSwitch = new MSwitch();
+	autoStartSwitch->setColor(QColor(40, 128, 185));
+	autoStartSwitch->setSizeOfSwitch(20);
 	auto switchLayout = new QHBoxLayout;
 	//switchLayout->addStretch();
 	switchLayout->addWidget(autoStartSwitch);
 	//switchLayout->addStretch();
 
 
-    autoLayout->addLayout(switchLayout);
-    autoLayout->addWidget(autostart);
+	autoLayout->addLayout(switchLayout);
+	autoLayout->addWidget(autostart);
 
-    bottomLayout->addWidget(startBtn);
-    bottomLayout->addSpacing(23);
-    bottomLayout->addWidget(coinType);
-    bottomLayout->addStretch();
-    bottomLayout->addLayout(autoLayout);
+	bottomLayout->addWidget(startBtn);
+	bottomLayout->addSpacing(23);
+	bottomLayout->addWidget(coinType);
+	bottomLayout->addStretch();
+	bottomLayout->addLayout(autoLayout);
 
-	layout->addSpacing(3);
-    layout->addLayout(bottomLayout);
-	layout->setSizeConstraint(QLayout::SetFixedSize);
-	layout->setSpacing(0);
 
-    setStyleSheet("#this{ background: rgba(17,17,17,1); margin:0px; padding : 0px; }"
-		"QScrollArea, #cardHolder{ border: 1px solid rgba(130,130,130,0); background: rgba(0,0,0,0); border-radius:1px; }"
-		"#cardHolder{background: rgba(0,0,0,0); padding: 0px; margin: 0px; }"
-                  "QLabel{ color: rgba(255,255,255,.9); }"
-                  "QToolButton {border-radius: 1px; background: rgba(20,20,20, 0); color: rgba(250,250,250, 1); border : 1px solid rgba(20,20,20, 0.1); padding: 4px 6px 4px 6px ; margin-right:3px;}"
-             //     "QToolButton:hover{background: rgba(48,48,48, 1);}"
-                  "QScrollBar::handle {background: rgba(40,128, 185,.9); border-radius: 4px; right: 1px; width: 8px;}"
-                  "QScrollBar{border : 0px solid black; background-color: rgba(32,32,32,.1); width: 8px;padding: 1px;}"
-                  " QScrollBar::add-page:vertical, QScrollBar::sub-page:vertical {background: rgba(200,200,200,0);}"
-                  "QScrollBar::up-arrow:vertical, QScrollBar::down-arrow:vertical { background: rgba(0,0,0,0); border: 0px solid white;}"
-                  "QScrollBar::sub-line, QScrollBar::add-line {background: rgba(10,0,0,.1);}"
-                  "QPushButton{ background: rgba(20,20,20,1); border: 1px solid rgba(10,10,10,1); border-radius: 1px;  color: rgba(255,255,255,.9); padding : 3px 9px 3px 9px; }"
-		""
-		"#startBtn{ padding: 9px 19px 9px 19px; background:rgba(23,23,23,.7); border:1px solid rgba(0,0,0,0);}"
-		"#startBtn:hover, QToolButton:hover { background : rgba(40,128, 185,.9); }"
-		"QScrollArea{background: rgba(23,23,23,1);}"
-		"#toolBar{ background: rgba(17,17,17,1); border-bottom: 0px solid black; }");
+	groupBoxLayout->addWidget(toolbar);
+	groupBoxLayout->addSpacing(3);
+	groupBoxLayout->addWidget(scrollArea);
+	groupBoxLayout->addSpacing(3);
+	groupBoxLayout->addLayout(bottomLayout);
+	//  groupBoxLayout->setSizeConstraint(QLayout::SetFixedSize);
+	groupBoxLayout->setSpacing(0);
+}
+
+void MinerUI::configureSettings()
+{
+	auto settingsWidget = new QWidget;
+	auto settingsLaout = new QGridLayout;
+	settingsLaout->setContentsMargins(5, 0, 5, 10);
+	settingsWidget->setLayout(settingsLaout);
+	settingsWidget->setObjectName(QStringLiteral("settingsWidget"));
+
+	auto toolbar = new QWidget;
+	toolbar->setObjectName(QStringLiteral("toolBar"));
+	toolbar->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred);
+	auto toolbarLayout = new QHBoxLayout;
+	toolbar->setLayout(toolbarLayout);
+
+	auto vHolder = new QWidget;
+	auto vLayout = new QVBoxLayout;
+	vHolder->setLayout(vLayout);
+	vHolder->setFixedSize(400, 200);
+	vLayout->addWidget(settingsWidget);
+	vLayout->setContentsMargins(10, 0, 10, 20);
+
+
+	back = new QPushButton("<-- back");
+	back->setObjectName(QStringLiteral("back"));
+	auto settingsLabel = new QLabel("SETTINGS");
+	settingsLabel->setAlignment(Qt::AlignHCenter);
+	QFont font = settingsLabel->font();
+	font.setBold(true);
+	settingsLabel->setFont(font);
+
+	auto settingsClose = new QPushButton("close");
+	settingsClose->setObjectName(QStringLiteral("back"));
+	connect(settingsClose, SIGNAL(clicked(bool)), close, SLOT(trigger()));
+
+	auto confirm = new QPushButton("Confirm");
+	auto CancelBtn = new QPushButton("Cancel");
+	confirm->setObjectName(QStringLiteral("bottomBtn"));
+	CancelBtn->setObjectName(QStringLiteral("bottomBtn"));
+
+	connect(confirm, &QPushButton::clicked, [=]() {
+		back->animateClick();
+	});
+	connect(CancelBtn, &QPushButton::clicked, [=]() {
+		back->animateClick();
+	});
+
+
+
+	toolbarLayout->addWidget(back);
+	toolbarLayout->addStretch();
+	toolbarLayout->addWidget(settingsLabel);
+	toolbarLayout->addStretch();
+	toolbarLayout->addWidget(settingsClose);
+
+
+
+	auto walletLabel = new QLabel("Wallet Id :");
+	auto password = new QLabel("Password :");
+	auto poolUrl = new QLabel("Pool URL :");
+	auto identifier = new QLabel("Identifier :");
+	auto currencyLabel = new QLabel("currency");
+	walletLabel->setObjectName(QStringLiteral("label"));
+	password->setObjectName(QStringLiteral("label"));
+	poolUrl->setObjectName(QStringLiteral("label"));
+	identifier->setObjectName(QStringLiteral("label"));
+	currencyLabel->setObjectName(QStringLiteral("label"));
+	walletLabel->setFont(font);
+	password->setFont(font);
+	poolUrl->setFont(font);
+	identifier->setFont(font);
+	currencyLabel->setFont(font);
+
+
+
+	currency = new QComboBox;
+	auto stringList = QStringList() << "Aeon" << "BBSCoin" << "Croat" << "Edollar" << "Electroneum" << "Graft" << "Haven" << "Intense" << "Karbo" << "Sumokoin" << "Monero7";
+	currency->setCurrentText("Select Currency");
+	currency->addItems(stringList);
+
+	settingsLaout->addWidget(toolbar, 0, 0, 1, 2);
+	// settingsLaout->add
+	settingsLaout->addWidget(walletLabel, 2, 0);
+	settingsLaout->addWidget(password, 3, 0);
+	settingsLaout->addWidget(poolUrl, 4, 0);
+	settingsLaout->addWidget(identifier, 5, 0);
+	settingsLaout->addWidget(currencyLabel, 6, 0);
+	settingsLaout->addWidget(currency, 6, 1);
+
+	settingsLaout->addWidget(confirm, 8, 0);
+	settingsLaout->addWidget(CancelBtn, 8, 1);
+
+	// settingsLaout->setSizeConstraint(QLayout::SetFixedSize);
+
+	stack->addWidget(vHolder);
+
+
+
 }
 
 void MinerUI::configureConnections()
 {
-   // connect(settings, SIGNAL(toggled(bool)),this,SLOT());
-    connect(advance, SIGNAL(triggered(bool)),this,SLOT(switchToAdvanceMode()));
-    connect(close,&QAction::triggered,[this](){
-        hide();
-    });
+	connect(settings, &QAction::triggered, [this]() {
+		stack->setCurrentIndex(1);
+	});
 
-    connect(startBtn, &QPushButton::clicked,[this](){
+	connect(back, &QPushButton::clicked, [this]() {
+		stack->setCurrentIndex(0);
 
-    });
-    connect(autoStartSwitch,&MSwitch::switchPressed,[this](bool val){
-        startAutomatically=val;
-    });
+	});
 
-    foreach (card, list){
-        connect(card,&GraphicsCardUI::switchIsOn,[this](bool val){
-           //do something;
-        });
-    }
+	connect(currency, QOverload<int>::of(&QComboBox::currentIndexChanged), [=](int index) {
+
+	});
+
+	connect(advance, SIGNAL(triggered(bool)), this, SLOT(switchToAdvanceMode()));
+	connect(close, &QAction::triggered, [this]() {
+		hide();
+	});
+
+	connect(startBtn, &QPushButton::clicked, [this]() {
+		foreach(card, list) card->setStarted();
+		startBtn->setText("Stop");
+
+	});
+	connect(autoStartSwitch, &MSwitch::switchPressed, [this](bool val) {
+		startAutomatically = val;
+	});
+
+	foreach(card, list) {
+		connect(card, &GraphicsCardUI::switchIsOn, [this](bool val) {
+			//do something;
+		});
+	}
+}
+
+void MinerUI::configureStyleSheet()
+{
+	setStyleSheet("QGroupBox, #settingsWidget{ background: rgba(33,33,33,1); margin:0px; padding : 0px; border: 0px solid black; }"
+		"QScrollArea, #cardHolder{ border: 0px solid rgba(130,130,130,0); background: rgba(17,17,17,0); border-radius:1px; }"
+		"#cardHolder{background: rgba(17,17,17,0); padding: 0px; margin: 0px; }"
+		"QLabel{ color: rgba(255,255,255,.9); }"
+		"QLabel#label{ padding-left: 10px; background:rgba(10,10,10,0); }"
+		"QToolButton {border-radius: 1px; background: rgba(20,20,20, 0); color: rgba(250,250,250, 1); border : 0px solid rgba(20,20,20, 1); padding: 4px 6px 4px 6px ; margin-right:3px;}"
+		//     "QToolButton:hover{background: rgba(48,48,48, 1);}"
+		"QScrollBar::handle {background: rgba(40,128, 185,.9); border-radius: 4px; right: 1px; width: 8px;}"
+		"QScrollBar{border : 0px solid black; background-color: rgba(32,32,32,.1); width: 8px;padding: 1px;}"
+		" QScrollBar::add-page:vertical, QScrollBar::sub-page:vertical {background: rgba(200,200,200,0);}"
+		"QScrollBar::up-arrow:vertical, QScrollBar::down-arrow:vertical { background: rgba(0,0,0,0); border: 0px solid white;}"
+		"QScrollBar::sub-line, QScrollBar::add-line {background: rgba(10,0,0,.1);}"
+		"QPushButton{ background: rgba(20,20,20,1); border: 1px solid rgba(10,10,10,1); border-radius: 1px;  color: rgba(255,255,255,.9); padding : 3px 9px 3px 9px; }"
+		""
+		"#startBtn{ padding: 9px 19px 9px 19px; background:rgba(23,23,23,.7); border:1px solid rgba(0,0,0,0);}"
+		"#startBtn:hover, QToolButton:hover, #back:hover { background : rgba(40,128, 185,.9); }"
+		//   "QScrollArea{background: rgba(23,23,23,1); border: 0px solid black; }"
+		"#toolBar{ background: rgba(40,128, 185,0); border: 1px solid rgba(10,0,0,0); }"
+		"#back{ background: rgba(40,128, 185,0); border: 0px solid rgba(40,40,40,0.3); }"
+		"#bottomBtn{border: 1px solid rgba(40,40,40,0.3); }"
+		"#bottomBtn:hover{background: rgba(40,128, 185,0.5);};"
+		"");
 }
 
 void MinerUI::switchToAdvanceMode()
 {
-    if(!isInAdvanceMode){
-        foreach (card, list)   card->expand();
-        isInAdvanceMode = true;
-    }
-    else{
-        foreach (card, list)   card->contract();
-        isInAdvanceMode = false;
-    }
+	if (!isInAdvanceMode) {
+		foreach(card, list)   card->expand();
+		isInAdvanceMode = true;
+	}
+	else {
+		foreach(card, list)   card->contract();
+		isInAdvanceMode = false;
+	}
 
 
 }
--- src/misc/minerui.h
+++ src/misc/minerui.h
@@ -13,8 +13,10 @@
 #include <QToolBar>
 #include <QDebug>
 #include <QGraphicsDropShadowEffect>
+#include <QStackedWidget>
+#include <QComboBox>
 #include "mswitch.h"
-#include "QtAwesome.h"
+//#include "QtAwesome.h"
 
 
 class QtAwesome;
@@ -22,7 +24,7 @@ class Dot : public QWidget {
 
 	Q_OBJECT
 public:
-	Dot(QColor col, QWidget *parent = Q_NULLPTR):QWidget(parent) {
+	Dot(QColor col, QWidget *parent = Q_NULLPTR) :QWidget(parent) {
 		setFixedSize(15, 15);
 		color = col;
 	}
@@ -46,168 +48,224 @@ private:
 class MSwitch;
 class GraphicsCardUI : public QWidget
 {
-    Q_OBJECT
+	Q_OBJECT
 public:
-    GraphicsCardUI(QWidget *parent = Q_NULLPTR) : QWidget(parent){
+	GraphicsCardUI(QWidget *parent = Q_NULLPTR) : QWidget(parent) {
 
-        configureCard();
-        configureConnections();
-        setColor(1);
+		configureCard();
+		configureConnections();
+		setColor(1);
 
-    }
+	}
 
-    void setCardName(QString name){
-        cardName->setText(name);
-        repaint();
-    }
+	void setCardName(QString name) {
+		cardName->setText(name);
+		repaint();
+	}
 
-    void expand(){
-        additional->show();
-    }
+	void expand() {
+		additional->show();
+	}
 
-    void contract(){
-        additional->hide();
-    }
+	void contract() {
+		additional->hide();
+	}
 
-    void setsSpeed(double rate){
-        speed->setText("Speed: "+ QString::number(rate));
-    }
+	void setsSpeed(double rate) {
+		speed->setText("Speed: " + QString::number(rate));
+	}
 
-    void setArmed(bool armed){
-        this->armed = armed;
-    }
+	void setArmed(bool armed) {
+		this->armed = armed;
+	}
 
 	void setDotColor(int con) {
 		setColor(con);
 		dot->setColor(color);
 	}
 
-      
+	void hideLabel() {
+		displayLabel->hide();
+	}
+
+	void showLabel() {
+		displayLabel->show();
+	}
+
+	void setStarted() {
+		if (mining) {
+			mining = false;
+
+		}
+
+	}
+
 
 private:
-    QWidget *logo, *info, *additional;
-    QColor color;
-    QLabel *cardName, *pool, *speed;
-    MSwitch *switchBtn;
+	QWidget * info, *additional;
+	QColor color;
+	QLabel *cardName, *pool, *speed, *displayLabel;
+	MSwitch *switchBtn;
 	Dot *dot;
-    bool armed;
+	QPushButton *logo;
+	bool armed, mining;
 
-    enum Connection{
-        CONNECTED = 1,
-        CONNECTING = 2,
-        NOTCONNECTED =3 ,
-        INACTIVE=4
+	enum Connection {
+		CONNECTED = 1,
+		CONNECTING = 2,
+		NOTCONNECTED = 3,
+		INACTIVE = 4
+	};
 
-    };
+	void setColor(int num) {
+		switch (num) {
+		case 1:
+			color.setRgb(0, 120, 0, 255);
+			break;
+		case 2:
+			color.setRgb(255, 120, 70, 255);
+			break;
+		case 3:
+			color.setRgb(170, 1, 2, 255);
+			break;
+		case 4:
+			color.setRgb(240, 240, 240, 255);
+			break;
+		}
 
-    void setColor(int num){
-        switch(num){
-        case 1:
-            color.setRgb(0,120,0,255);
-            break;
-        case 2:
-            color.setRgb(255,120,70,255);
-            break;
-        case 3:
-            color.setRgb(170,1,2,255);
-            break;
-        case 4:
-            color.setRgb(240,240,240,255);
-            break;
-        }
-	
-    }
+	}
 
-    void configureCard(){
-        QVBoxLayout *cardLayout = new QVBoxLayout;
-        QHBoxLayout *mainLayout = new QHBoxLayout;
-        QHBoxLayout *sliderLayout = new QHBoxLayout;
-        QVBoxLayout *logoLayout = new QVBoxLayout;
-        QVBoxLayout *infoLayout = new QVBoxLayout;
+	void configureCard() {
+		// QVBoxLayout *cardLayout = new QVBoxLayout;
+		QHBoxLayout *mainLayout = new QHBoxLayout;
+		QHBoxLayout *sliderLayout = new QHBoxLayout;
+		QVBoxLayout *logoLayout = new QVBoxLayout;
+		QVBoxLayout *infoLayout = new QVBoxLayout;
 		auto poolDotLayout = new QHBoxLayout;
-		
 
-        logo = new QWidget();
-        logo->setObjectName(QStringLiteral("logo"));
-        logo->setLayout(logoLayout);
-        logo->setFixedSize(100,98);
+		auto card = new QWidget;
+		auto cardLayout = new QGridLayout;
+		card->setLayout(cardLayout);
+		card->setObjectName(QStringLiteral("card"));
 
-        pool = new QLabel("Pool  ");
-        pool->setAlignment(Qt::AlignHCenter);
+		auto toolBar = new QToolBar;
+		toolBar->setFixedWidth(450);
+
+		logo = new QPushButton();
+		logo->setObjectName(QStringLiteral("logo"));
+		logo->setLayout(logoLayout);
+		logo->setFixedSize(100, 98);
+		logo->setCheckable(true);
+		logo->setCursor(Qt::PointingHandCursor);
+
+
+		pool = new QLabel("Pool  ");
+		pool->setAlignment(Qt::AlignHCenter);
 		dot = new Dot(color);
 		poolDotLayout->addWidget(pool);
 		poolDotLayout->addWidget(dot);
 		poolDotLayout->setSpacing(2);
 		setDotColor(CONNECTED);
 
-        speed = new QLabel("Speed: ");
-        speed->setAlignment(Qt::AlignHCenter);
-        cardName = new QLabel("AMD A9");
-        cardName->setAlignment(Qt::AlignHCenter);
+		speed = new QLabel("Speed: ");
+		speed->setAlignment(Qt::AlignHCenter);
+		cardName = new QLabel("AMD A9");
+		cardName->setAlignment(Qt::AlignHCenter);
+		cardName->setWordWrap(true);
 
-        QFont font = cardName->font();
-        font.setStyleStrategy(QFont::PreferAntialias);
-        font.setBold(true);
-        cardName->setFont(font);
-        font.setBold(false);
+		QFont font = cardName->font();
+		font.setStyleStrategy(QFont::PreferAntialias);
+		font.setBold(true);
+		cardName->setFont(font);
+		font.setBold(false);
 		font.setPixelSize(12);
-        pool->setFont(font);
-        speed->setFont(font);
+		pool->setFont(font);
+		speed->setFont(font);
 
-        switchBtn = new MSwitch();
-        switchBtn->setColor(QColor(40, 128, 185));
-        switchBtn->setSizeOfSwitch(22);
-        sliderLayout->addStretch();
-        sliderLayout->addWidget(switchBtn);
-        sliderLayout->addStretch();
+		switchBtn = new MSwitch();
+		switchBtn->setColor(QColor(40, 128, 185));
+		switchBtn->setSizeOfSwitch(22);
+		sliderLayout->addStretch();
+		sliderLayout->addWidget(switchBtn);
+		sliderLayout->addStretch();
 
-		logoLayout->addSpacing(4);
-        logoLayout->addWidget(cardName);
+		logoLayout->addStretch();
+		logoLayout->addWidget(cardName);
 		logoLayout->addLayout(poolDotLayout);
-        logoLayout->addWidget(speed);
-        logoLayout->addLayout(sliderLayout);
-		logoLayout->addSpacing(4);
+		logoLayout->addWidget(speed);
+		// logoLayout->addLayout(sliderLayout);
+		logoLayout->addStretch();
 
-        info = new QWidget();
-        info->setObjectName(QStringLiteral("info"));
-        info->setLayout(infoLayout);
-        info->setFixedSize(350,98);
+		info = new QWidget();
+		info->setObjectName(QStringLiteral("info"));
+		info->setLayout(infoLayout);
+		info->setFixedHeight(98);
+		info->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
 
-        additional = new QWidget();
-        additional->setObjectName(QStringLiteral("additional"));    
-        additional->setMinimumHeight(80);
-        additional->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+		additional = new QWidget();
+		additional->setObjectName(QStringLiteral("additional"));
+		additional->setMinimumHeight(80);
+		additional->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
 
-        mainLayout->addWidget(logo);
-        mainLayout->addWidget(info);
-        cardLayout->addLayout(mainLayout);
-        cardLayout->addWidget(additional);
-		cardLayout->setSpacing(6);
-		cardLayout->setContentsMargins(2, 3, 3, 2);
-        setLayout(cardLayout);
-        setStyleSheet(" QWidget#logo, QWidget#info,QWidget#additional  { background:rgba(17,17,17,1); border : 1px solid rgba(00,00,00,.2); border-radius: 1px; margin: 0px;  }"
-			"QWidget#logo:hover, QWidget#info:hover,QWidget#additional:hover{border : 1px solid rgba(40,128,185,.2); }"
-                      "QLabel{ color:rgba(255,255,255,.8); }");
+		displayLabel = new QLabel("GPU is not set to mined");
+		displayLabel->setAlignment(Qt::AlignHCenter);
+		displayLabel->setObjectName(QStringLiteral("gpuLabel"));
+		font.setBold(true);
+		displayLabel->setFont(font);
+		infoLayout->addWidget(displayLabel);
 
-        QGraphicsDropShadowEffect *effect = new QGraphicsDropShadowEffect;
-            effect->setBlurRadius(10);
-            effect->setXOffset(0);
-            effect->setYOffset(1);
-        effect->setColor(QColor(10,10,10));
-        //setGraphicsEffect(effect);
-        //additional->setGraphicsEffect(effect);
-        //logo->setGraphicsEffect(effect);
-    }
+		toolBar->addWidget(logo);
+		toolBar->addSeparator();
+		toolBar->addWidget(info);
 
-    void configureConnections(){
-        connect(switchBtn, &MSwitch::switchPressed,[this](bool val){
-            emit switchIsOn(val);
-            armed = val;
-        });
-    }
+		cardLayout->addWidget(toolBar);
+		cardLayout->addWidget(additional);
+
+		// mainLayout->addWidget(logo);
+		// mainLayout->addWidget(info);
+		//        cardLayout->addLayout(mainLayout);
+		//        cardLayout->addWidget(additional);
+		//        cardLayout->setSpacing(6);
+		cardLayout->setSizeConstraint(QLayout::SetFixedSize);
+		cardLayout->setContentsMargins(2, 1, 3, 2);
+		setLayout(mainLayout);
+		mainLayout->addWidget(card);
+
+		qDebug() << card->geometry();
+		setStyleSheet(" QWidget#logo, QWidget#info,QWidget#additional  { background:rgba(17,17,17,0); border : 0px solid rgba(00,00,00,.2); border-radius: 1px; margin: 0px;  }"
+			"QWidget#logo:hover, QWidget#info:hover,QWidget#additional:hover{border : 1px solid rgba(40,128,185,.01); }"
+			"QLabel{ color:rgba(255,255,255,.8); padding :3px; }"
+			"#logo:checked {background: qlineargradient(x1: 0, y1: 1, x2: 1, y2: 1, stop: 0 rgba(40,128,185,.5), stop: 0.5 rgba(17,17,17,0));}"
+			"QToolBar::separator{ background:rgba(0,0,0,.1);}"
+			"#card{background: rgba(40,128,185,.1); border: 3px solid rgba(0,0,0,.85);}"
+			"#gpuLabel{ color: rgba(150,150,170,.8);}");
+
+		QGraphicsDropShadowEffect *effect = new QGraphicsDropShadowEffect;
+		effect->setBlurRadius(10);
+		effect->setXOffset(0);
+		effect->setYOffset(0);
+		effect->setColor(QColor(0, 0, 0));
+		//  card->setGraphicsEffect(effect);
+		//additional->setGraphicsEffect(effect);
+		//logo->setGraphicsEffect(effect);
+	}
+
+	void configureConnections() {
+		connect(switchBtn, &MSwitch::switchPressed, [this](bool val) {
+			emit switchIsOn(val);
+			armed = val;
+		});
+
+		connect(logo, &QPushButton::clicked, [this]() {
+			emit switchIsOn(logo->isChecked());
+			armed = logo->isChecked();
+			if (logo->isChecked())  displayLabel->setText("GPU set to mined");
+			else displayLabel->setText("GPU is not set to mine");
+		});
+	}
 
 signals:
-    void switchIsOn(bool);
+	void switchIsOn(bool);
 
 };
 
@@ -218,35 +276,40 @@ signals:
 
 class MinerUI : public QWidget
 {
-    Q_OBJECT    
+	Q_OBJECT
 public:
-    MinerUI(QWidget *parent = 0);
-    ~MinerUI();
-    void addGraphicsCard(QString string);
-    bool setToStartAutomatically(){
-        return startAutomatically;
-    }
-private slots:
-    void switchToAdvanceMode();
+	MinerUI(QWidget *parent = 0);
+	~MinerUI();
+	void addGraphicsCard(QString string);
+	bool setToStartAutomatically() {
+		return startAutomatically;
+	}
+	private slots:
+	void switchToAdvanceMode();
 
 private:
-    void configureUI();
-    void configureConnections();
+	void configureUI();
+	void configureSettings();
+	void configureConnections();
+	void configureStyleSheet();
 
-    bool isInAdvanceMode=true, startAutomatically;
-    QList<GraphicsCardUI *> list;
-    QToolBar *toolbar;
-    QVBoxLayout *layout;
-    QVBoxLayout *cardHolderLayout;
-    QScrollArea *scrollHolder;
-    QWidget *cardHolder;
-    GraphicsCardUI *card;
-    QPushButton *startBtn;
-    QLabel *coinType, *autostart;
-    QAction *settings, *close;
-    QAction *advance;
-    MSwitch *autoStartSwitch;
-	QtAwesome fontIcon;
+	bool isInAdvanceMode = true, startAutomatically, mining;
+	QStackedWidget *stack;
+	QList<GraphicsCardUI *> list;
+	QToolBar *toolbar;
+	QGridLayout *layout;
+	QVBoxLayout *cardHolderLayout;
+	QScrollArea *scrollArea;
+	QWidget *cardHolder;
+	GraphicsCardUI *card;
+	QPushButton *startBtn;
+	QLabel *coinType, *autostart;
+	QAction *settings, *close;
+	QAction *advance;
+	MSwitch *autoStartSwitch;
+	QPushButton *back;
+	QComboBox *currency;
+	//QtAwesome fontIcon;
 };
 
 #endif // MINERUI_H
