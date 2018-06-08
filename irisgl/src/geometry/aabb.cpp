82debd632b34927157157fdd98355afc7fbdd2e0
Nicolas Brown <nclsbrwn@gmail.com>
2018-06-08 14:24:12 -0500

    Calculating AABB from mesh

--- irisgl/src/geometry/aabb.cpp
+++ irisgl/src/geometry/aabb.cpp
@@ -1,5 +1,9 @@
 #include "aabb.h"
+#include "boundingsphere.h"
 #include <limits>
+
+namespace iris
+{
 
 AABB::AABB()
 {
@@ -10,12 +14,12 @@ AABB::AABB()
 void AABB::setNegativeInfinity()
 {
 	minPos = QVector3D(std::numeric_limits<float>::max(),
-					   std::numeric_limits<float>::max(),
-					   std::numeric_limits<float>::max());
+		std::numeric_limits<float>::max(),
+		std::numeric_limits<float>::max());
 
 	maxPos = QVector3D(-std::numeric_limits<float>::max(),
-					   -std::numeric_limits<float>::max(),
-					   -std::numeric_limits<float>::max());
+		-std::numeric_limits<float>::max(),
+		-std::numeric_limits<float>::max());
 }
 
 QVector3D AABB::getMin(QVector3D a, QVector3D b)
@@ -40,6 +44,23 @@ QVector3D AABB::getMax(QVector3D a, QVector3D b)
 	return result;
 }
 
+QVector3D AABB::getCenter()
+{
+	return (minPos + maxPos) * 0.5f;
+}
+
+QVector3D AABB::getSize()
+{
+	return QVector3D(maxPos.x() - minPos.x(),
+		maxPos.y() - minPos.y(),
+		maxPos.z() - minPos.z());
+}
+
+QVector3D AABB::getHalfSize()
+{
+	return getSize() * 0.5f;
+}
+
 void AABB::merge(QVector3D point)
 {
 	minPos = getMin(minPos, point);
@@ -51,4 +72,11 @@ void AABB::merge(QVector<QVector3D> points)
 	for (auto &p : points) {
 		merge(p);
 	}
+}
+
+BoundingSphere AABB::getMinimalEnclosingSphere()
+{
+	return { getCenter(), getSize().length() * 0.5f };
+}
+
 }
--- irisgl/src/geometry/aabb.h
+++ irisgl/src/geometry/aabb.h
@@ -1,20 +1,35 @@
+#pragma once
 #include <QVector>
 #include <QVector3D>
+#include "boundingsphere.h"
+
+namespace iris
+{
 
 class AABB
 {
-	 QVector3D minPos;
-	 QVector3D maxPos;
+	QVector3D minPos;
+	QVector3D maxPos;
 
 public:
-	 AABB();
+	AABB();
 
-	 void setNegativeInfinity();
-	 QVector3D getMin(QVector3D a, QVector3D b);
-	 QVector3D getMax(QVector3D a, QVector3D b);
+	void setNegativeInfinity();
 
-	 void merge(QVector3D point);
-	 void merge(QVector<QVector3D> points);
+	QVector3D getCenter();
+	QVector3D getSize();
+	QVector3D getHalfSize();
 
-	 static AABB fromPoints(QVector<QVector3D> points);
+	void merge(QVector3D point);
+	void merge(QVector<QVector3D> points);
+
+	BoundingSphere getMinimalEnclosingSphere();
+
+	static AABB fromPoints(QVector<QVector3D> points);
+
+private:
+	QVector3D getMin(QVector3D a, QVector3D b);
+	QVector3D getMax(QVector3D a, QVector3D b);
 };
+
+}
