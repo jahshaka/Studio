b50cbfc430144f9406b378b512084a212a1a4c41
Unknown <willroymyles@yahoo.com>
2018-06-13 16:25:01 -0500

    Added license to applicable files
    Added license to shaders

--- src/commands/changematerialpropertycommand.cpp
+++ src/commands/changematerialpropertycommand.cpp
@@ -1,3 +1,14 @@
+/**************************************************************************
+This file is part of JahshakaVR, VR Authoring Toolkit
+http://www.jahshaka.com
+Copyright (c) 2016  GPLv3 Jahshaka LLC <coders@jahshaka.com>
+
+This is free software: you may copy, redistribute
+and/or modify it under the terms of the GPLv3 License
+
+For more information see the LICENSE file
+*************************************************************************/
+
 #include "changematerialpropertycommand.h"
 #include "../irisgl/src/core/property.h"
 #include "../irisgl/src/materials/custommaterial.h"
