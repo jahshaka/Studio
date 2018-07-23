0694ac380f4ae1d04a2ce35c9968b153d77d4010
Unknown <willroymyles@yahoo.com>
2018-07-23 15:52:16 -0500

    Rename Jahshaka to Jashaka Studio

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -158,7 +158,7 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
 
 #ifdef QT_DEBUG
     iris::Logger::getSingleton()->init(IrisUtils::getAbsoluteAssetPath("jahshaka.log"));
-    setWindowTitle(QString("Jahshaka %1 - %2").arg(Constants::CONTENT_VERSION).arg("Developer Build"));
+    setWindowTitle(QString("Jahshaka Studio %1 - %2").arg(Constants::CONTENT_VERSION).arg("Developer Build"));
 #else
 	setWindowTitle(QString("Jahshaka %1").arg(Constants::CONTENT_VERSION));
     iris::Logger::getSingleton()->init(QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation)+"/jahshaka.log");
