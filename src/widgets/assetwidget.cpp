d561c1ba40bc4859cdffc5fdb14b5e2ada2adf68
Nicolas Brown <nclsbrwn@gmail.com>
2018-12-13 17:39:30 -0500

    Removed instances of DefaultAnimatedShader

--- src/widgets/assetwidget.cpp
+++ src/widgets/assetwidget.cpp
@@ -329,11 +329,7 @@ void AssetWidget::trigger()
 				[&](iris::MeshPtr mesh, iris::MeshMaterialData& data)
 			{
 				auto mat = iris::CustomMaterial::create();
-
-				if (mesh->hasSkeleton())
-					mat->generate(IrisUtils::getAbsoluteAssetPath("app/shader_defs/DefaultAnimated.shader"));
-				else
-					mat->generate(IrisUtils::getAbsoluteAssetPath("app/shader_defs/Default.shader"));
+				mat->generate(IrisUtils::getAbsoluteAssetPath("app/shader_defs/Default.shader"));
 
 				return mat;
 			});
@@ -1926,10 +1922,7 @@ void AssetWidget::importJafAssets(const QList<directory_tuple> &fileNames)
                         [&](iris::MeshPtr mesh, iris::MeshMaterialData& data)
                     {
                         auto mat = iris::CustomMaterial::create();
-                        if (mesh->hasSkeleton())
-                            mat->generate(IrisUtils::getAbsoluteAssetPath("app/shader_defs/DefaultAnimated.shader"));
-                        else
-                            mat->generate(IrisUtils::getAbsoluteAssetPath("app/shader_defs/Default.shader"));
+                        mat->generate(IrisUtils::getAbsoluteAssetPath("app/shader_defs/Default.shader"));
                         return mat;
                     }, ssource);
                     this->sceneView->doneCurrent();
