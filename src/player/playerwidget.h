3562d55a67cf481745869b818867a6c007db131e
Nicolas Brown <nclsbrwn@gmail.com>
2019-02-01 23:39:16 -0500

    Start splitting player into it's own widget

--- src/player/playerwidget.h
+++ src/player/playerwidget.h
@@ -1,66 +1,20 @@
-#pragma once
+#ifndef PLAYERWIDGET_H
+#define PLAYERWIDGET_H
 
-#include <QOpenGLBuffer>
-#include <QHash>
-#include <QMatrix4x4>
-#include <QOpenGLFunctions>
-#include <QOpenGLFunctions_3_2_Core>
-#include <QOpenGLWidget>
-#include <QSharedPointer>
+#include <QWidget>
+#include <QObject>
 
-#include "irisgl/src/bullet3/src/btBulletDynamicsCommon.h" 
+class PlayerView;
 
-#include "irisgl/src/irisglfwd.h"
-#include "irisgl/src/math/intersectionhelper.h"
-#include "irisgl/src/physics/physicsproperties.h"
-
-#include "mainwindow.h"
-#include "uimanager.h"
-
-#include "core/project.h"
-
-namespace iris
-{
-	class CameraNode;
-	class DefaultMaterial;
-	class ForwardRenderer;
-	class FullScreenQuad;
-	class Mesh;
-	class MeshNode;
-	class Scene;
-	class SceneNode;
-	class Viewport;
-	class ContentManager;
-}
-
-class CameraControllerBase;
-class EditorCameraController;
-class EditorData;
-class EditorVrController;
-class Gizmo;
-class OrbitalCameraController;
-class OutlinerRenderer;
-class QElapsedTimer;
-class QOpenGLDebugLogger;
-class QOpenGLShaderProgram;
-class QTimer;
-class RotationGizmo;
-class ScaleGizmo;
-class ThumbnailGenerator;
-class OutlinerRenderer;
-class AnimationPath;
-class TranslationGizmo;
-class ViewerCameraController;
-class ViewportGizmo;
-class Globals;
-class btRigidBody;
-
-class PlayerWidget : public QOpenGLWidget, protected QOpenGLFunctions_3_2_Core
+class PlayerWidget : public QWidget
 {
 	Q_OBJECT
 
-	iris::CameraNodePtr editorCam;
+	PlayerView* playerView;
 public:
-	PlayerWidget(QWidget* parent = nullptr);
-	~PlayerWidget();
+	explicit PlayerWidget(QWidget* parent = nullptr);
+	~PlayerWidget() {}
+	void createUI();
 };
+
+#endif PLAYERWIDGET_H
