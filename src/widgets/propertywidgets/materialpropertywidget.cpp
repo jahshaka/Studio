7debc8e1f63ac332f0fcbeb5f60a8c032e7bd94d
unknown <willroymyles@yahoo.com>
2019-02-05 13:11:04 -0600

    Shaders now applied when sleceted from paterial property panel

--- src/widgets/propertywidgets/materialpropertywidget.cpp
+++ src/widgets/propertywidgets/materialpropertywidget.cpp
@@ -12,6 +12,7 @@ For more information see the LICENSE file
 #include "materialpropertywidget.h"
 
 #include <QJsonObject>
+#include <QDirIterator>
 
 #include "../accordianbladewidget.h"
 #include "../hfloatsliderwidget.h"
@@ -161,6 +162,19 @@ void MaterialPropertyWidget::materialChanged(int index)
             Globals::project->getProjectGuid()
         );
     }
+
+	
+	for (auto prop : material->properties) {
+		if (prop->type == (iris::PropertyType::Texture)) {
+			auto guid = prop->getValue().toString();
+
+			auto asset = db->fetchAsset(guid).name;
+			auto path = QDir(Globals::project->getProjectFolder()).filePath(asset);
+
+			if(QFile::exists(path))		material->setTextureWithUniform(prop->uniform, path);
+
+		}
+	}
 }
 
 void MaterialPropertyWidget::setupShaderSelector()
