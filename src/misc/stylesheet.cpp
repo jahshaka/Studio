1d91601bb1a48ee3ee6fa123d514ede7a9a019f8
Unknown <willroymyles@yahoo.com>
2019-02-21 12:24:24 -0600

    Made stylesheet return explicit QString
    - Replaced authorship head
    - Made all functions return const QString

--- src/misc/stylesheet.cpp
+++ src/misc/stylesheet.cpp
@@ -1,30 +1,34 @@
-//
-//  SS.cpp
-//  color_picker
-//
-//  Created by will on 2/3/19.
-//
+/**************************************************************************
+This file is part of JahshakaVR, VR Authoring Toolkit
+http://www.jahshaka.com
+Copyright (c) 2016  GPLv3 Jahshaka LLC <coders@jahshaka.com>
+
+This is free software: you may copy, redistribute
+and/or modify it under the terms of the GPLv3 License
+
+For more information see the LICENSE file
+*************************************************************************/
 
 #include "stylesheet.h"
 
 
 
-QString StyleSheet::QPushButtonInvisible() {
-    return {
+const QString StyleSheet::QPushButtonInvisible() {
+    return QString(
         "QPushButton{background : rgba(0,0,0,0); border : 0px; }"
-    };
+    );
 }
 
-QString StyleSheet::QSpinBox() {
-    return {
-        "QAbstractSpinBox { padding-right: 0px;    background: #292929; margin-right: -6px; }"
-        "QAbstractSpinBox, QLabel { color:rgba(255,255,255,.8); padding: 3px } QAbstractSpinBox::down-button, QAbstractSpinBox::up-button { background: rgba(0,0,0,0); border : 1px solid rgba(0,0,0,0); }"
+const QString StyleSheet::QSpinBox() {
+    return QString(
+        "QAbstractSpinBox {  color:rgba(255,255,255,.8); padding: 3px; padding-right: 0px;    background: #292929; margin-right: -6px; }"
+		"QAbstractSpinBox::down-button, QAbstractSpinBox::up-button { background: rgba(0,0,0,0); border : 1px solid rgba(0,0,0,0); }"
         
-    };
+    );
 }
 
-QString StyleSheet::QSlider() {
-    return {
+const QString StyleSheet::QSlider() {
+    return QString(
         "QSlider::sub-page {    border: 0px solid transparent;    height: 2px;    background: #3498db;    margin: 2px 0;}"
         "QSlider::groove:horizontal { border: 0px solid transparent; height: 4px; background: #1e1e1e;   margin: 1px 0; border-radius: .5px; }"
         "QSlider::handle:horizontal {    background-color: #CCC;    width: 12px;    border: 1px solid #1e1e1e;    margin: -5px 0px;   border-radius:7px;}"
@@ -34,61 +38,60 @@ QString StyleSheet::QSlider() {
         " QSlider::handle:vertical {height: 3px; width:1px; margin: -2px 0px; background: rgba(50,148,213,0.9); }"
         " QSlider::add-page:vertical, QSlider::sub-page:vertical {background: rgba(0,0,0,0); border-radius: 1px;}"
         
-    };
+    );
 }
 
-QString StyleSheet::QLineEdit() {
-    return {
+const QString StyleSheet::QLineEdit() {
+    return QString(
         "QLineEdit { color: rgba(255,255,255,.9); background: rgba(51,51,51,0.5); border: 1px solid rgba(0,0,0,.4); selection-background-color: #808080; padding : 5px;}"
 
-    };
+    );
 }
 
-QString StyleSheet::QPushButtonGreyscale() {
-    return{
+const QString StyleSheet::QPushButtonGreyscale() {
+    return QString(
         "QPushButton{ background-color: #333; color: #DEDEDE; border : 0; padding: 4px 16px; }"
         "QPushButton:hover{ background-color: #555; }"
         "QPushButton:pressed{ background-color: #444; }"
         "QPushButton:checked{border: 0px solid rgba(0,0,0,.3); background: rgba(50,148,213,0.9); color: rgba(255,255,255,.9); }"
-    };
+    );
 }
 
-QString StyleSheet::QWidgetDark() {
-    return {
+const QString StyleSheet::QWidgetDark() {
+    return QString(
         "QWidget{ background: rgba(26,26,26,1);border: 1px solid rgba(0,0,0,0); padding:0px; spacing : 0px;}"
 
-    };
+    );
 }
 
-QString StyleSheet::QLabelWhite() {
-    return {
+const QString StyleSheet::QLabelWhite() {
+    return QString(
         "QLabel{ color : rgba(255, 255,255, .9); }"
-    };
+    );
 }
 
-QString StyleSheet::QComboBox() {
-    return {
+const QString StyleSheet::QComboBox() {
+    return QString(
         "QComboBox{background: rgba(0,0,0,0); border :0px; border-bottom: 1px solid black; padding: 5px; margin-left : 5px; color : rgba(255,255,255,.9);} "
         "QComboBox:drop-down {   border :0px solid black;}"
         "QComboBox QAbstractItemView { background-color: rgba(33,33,33,1);  border :0px; border-bottom: 1px solid black;padding: 5px; padding-left: 1px; margin-left : 0px; outline: 0px; selection-background-color: rgba(40,128, 185,0); }"
         "QComboBox QAbstractItemView::item:hover { background-color: red; border :0px solid rgba(0,0,0,0);}"
-    };
+    );
 }
 
-QString StyleSheet::QPushButtonRounded(int size) {
-    return {
-        "QPushButton{border : 0px; radius : "+QString::number(size/2)+" }"
-    };
+const QString StyleSheet::QPushButtonRounded(int size) {
+    return QString(
+        "QPushButton{border : 0px; radius : "+ QString::number(size/2)+" }"
+    );
 }
 
-QString StyleSheet::QPushButtonGrouped() {
+const QString StyleSheet::QPushButtonGrouped() {
     return QString(
     "QPushButton{ background:rgba(51,51,51,.5); color: rgba(190,190,190,1); border : 1px solid rgba(20,20,20,.4); padding: 5px 16px;} "
     "QPushButton:checked{ color : rgba(50,150,250,1);}"
     "QPushButton:hover{background: rgba(50,50,50,1);}"
     "QPushButton:pressed{background:rgba(61,61,61,.9);}"
-                                                                                                 
-                                                                                                 );
+    );
 }
 
 
--- src/misc/stylesheet.h
+++ src/misc/stylesheet.h
@@ -1,25 +1,29 @@
-//
-//  SS.hpp
-//  color_picker
-//
-//  Created by will on 2/3/19.
-//
+/**************************************************************************
+This file is part of JahshakaVR, VR Authoring Toolkit
+http://www.jahshaka.com
+Copyright (c) 2016  GPLv3 Jahshaka LLC <coders@jahshaka.com>
+
+This is free software: you may copy, redistribute
+and/or modify it under the terms of the GPLv3 License
+
+For more information see the LICENSE file
+*************************************************************************/
 
 #include <QWidget>
 
 class StyleSheet : public QObject
 {
 public:
-    static QString QPushButtonInvisible();
-    static QString QPushButtonGreyscale();
-    static QString QPushButtonRounded(int size);
-    static QString QSpinBox();
-    static QString QSlider();
-    static QString QLineEdit();
-    static QString QWidgetDark();
-    static QString QLabelWhite();
-    static QString QComboBox();
-    static QString QPushButtonGrouped();
+    static const QString QPushButtonInvisible();
+    static const QString QPushButtonGreyscale();
+    static const QString QPushButtonRounded(int size);
+    static const QString QSpinBox();
+    static const QString QSlider();
+    static const QString QLineEdit();
+    static const QString QWidgetDark();
+    static const QString QLabelWhite();
+    static const QString QComboBox();
+    static const QString QPushButtonGrouped();
 };
 
 
