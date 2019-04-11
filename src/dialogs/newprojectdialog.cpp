4a7e95667e1f89eb19e4a72ea8f01c559f489823
Ricardo Lovelace <iklsr@outlook.com>
2019-04-11 15:29:57 -0500

    Fix NewProjectDialog style and reuse

--- src/dialogs/newprojectdialog.cpp
+++ src/dialogs/newprojectdialog.cpp
@@ -27,32 +27,25 @@ NewProjectDialog::NewProjectDialog(QDialog *parent) : QDialog(parent), ui(new Ui
 {
     ui->setupUi(this);
 
-    this->setWindowTitle("New World");
+	setAttribute(Qt::WA_MacShowFocusRect, false);
+	setWindowFlags(windowFlags() | Qt::WindowStaysOnTopHint | Qt::FramelessWindowHint);
 
     ui->createProject->setAutoDefault(true);
     ui->createProject->setDefault(true);
-
     ui->projectName->setAttribute(Qt::WA_MacShowFocusRect, false);
 
     settingsManager = SettingsManager::getDefaultManager();
-
-    ui->projectPath->setDisabled(true);
+	
+	ui->projectPath->setDisabled(true);
     ui->projectPath->setStyleSheet("background: #303030; color: #888;");
 
-//    connect(ui->browseProject, SIGNAL(pressed()), SLOT(setProjectPath()));
     connect(ui->createProject, SIGNAL(pressed()), SLOT(confirmProjectCreation()));
     connect(ui->cancel, SIGNAL(pressed()), SLOT(close()));
 
-//    lastValue = settingsManager->getValue("last_wd", "").toString();
-//    if (!lastValue.isEmpty()) {
-//        projectPath = lastValue;
-//        ui->projectPath->setText(lastValue);
-//    } else {
-        auto path = QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation)
-                    + Constants::PROJECT_FOLDER;
-        projectPath = settingsManager->getValue("default_directory", path).toString();
-        ui->projectPath->setText(projectPath);
-//    }
+    auto path = QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation) + Constants::PROJECT_FOLDER;
+    projectPath = settingsManager->getValue("default_directory", path).toString();
+
+    ui->projectPath->setText(projectPath);
 }
 
 NewProjectDialog::~NewProjectDialog()
@@ -80,18 +73,7 @@ void NewProjectDialog::createNewProject()
 
 void NewProjectDialog::confirmProjectCreation()
 {
-    //if (QDir(projectPath + '/' + ui->projectName->text()).exists()) {
-    //    QMessageBox::StandardButton reply;
-    //    reply = QMessageBox::question(this, "Project Path not Empty", "Project already Exists! Overwrite?",
-    //                                    QMessageBox::Yes|QMessageBox::No);
-    //    if (reply == QMessageBox::Yes) {
-    //        createNewProject();
-    //        this->close();
-    //        emit accepted();
-    //    }
-    //} else {
-        createNewProject();
-        this->close();
-        emit accepted();
-    //}
+    createNewProject();
+    this->close();
+    emit accepted();
 }
