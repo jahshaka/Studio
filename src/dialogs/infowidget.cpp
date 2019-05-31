4d30173953b73ae41dbd3fb2f902081e89b08cb7
wll <willroymyles@yahoo.com>
2019-05-31 14:21:43 -0700

    Aligned letters in display

--- src/dialogs/infowidget.cpp
+++ src/dialogs/infowidget.cpp
@@ -35,10 +35,35 @@ void InfoWidget::editorMenu(QKeyEvent *event)
 void InfoWidget::configureEditorMenu()
 {
 	auto wid = new QWidget;
-	auto lay = new QVBoxLayout;
+	auto lay = new QGridLayout;
 	wid->setLayout(lay);
-	lay->addWidget(new QLabel("Access Key.....q"));
-	lay->addWidget(new QLabel("T ..... Translate"));
+	lay->addWidget(new QLabel("Access Key.....q"),0,1,1,2);
+	lay->addWidget(new QLabel("Any other key to dismiss"), 20, 1, 1, 2);
+
+	lay->addWidget(new QLabel("T"), 1, 1, 1, 1);
+	lay->addWidget(new QLabel(".....Translate"), 1, 2, 1, 1);
+	lay->addWidget(new QLabel("R"), 2, 1, 1, 1);
+	lay->addWidget(new QLabel(".....Rotate"), 2, 2, 1, 1);
+	lay->addWidget(new QLabel("S"), 3, 1, 1, 1);
+	lay->addWidget(new QLabel(".....Scale"), 3, 2, 1, 1);
+	lay->addWidget(new QLabel("G"), 4, 1, 1, 1);
+	lay->addWidget(new QLabel(".....Global Space"), 4, 2, 1, 1);
+	lay->addWidget(new QLabel("L"), 5, 1, 1, 1);
+	lay->addWidget(new QLabel(".....Local Space"), 5, 2, 1, 1);
+	lay->addWidget(new QLabel("F"), 6, 1, 1, 1);
+	lay->addWidget(new QLabel(".....Freeball Camera"), 6, 2, 1, 1);
+	lay->addWidget(new QLabel("A"), 7, 1, 1, 1);
+	lay->addWidget(new QLabel(".....Arcball Camera"), 7, 2, 1, 1);
+	lay->addWidget(new QLabel("O"), 8, 1, 1, 1);
+	lay->addWidget(new QLabel(".....Othragonal View"), 8, 2, 1, 1);
+	lay->addWidget(new QLabel("P"), 9, 1, 1, 1);
+	lay->addWidget(new QLabel(".....Perspective View"), 9, 2, 1, 1);
+
+	
+
+
+
+	/*lay->addWidget(new QLabel("T ..... Translate"));
 	lay->addWidget(new QLabel("R ..... Rotate"));
 	lay->addWidget(new QLabel("S ..... Scale"));
 	lay->addWidget(new QLabel("G ..... Global Space"));
@@ -46,11 +71,11 @@ void InfoWidget::configureEditorMenu()
 	lay->addWidget(new QLabel("F ..... Freeball Camera"));
 	lay->addWidget(new QLabel("A ..... Arcball camera"));
 	lay->addWidget(new QLabel("O ..... Othragonal View"));
-	lay->addWidget(new QLabel("P ..... Perspective View"));
+	lay->addWidget(new QLabel("P ..... Perspective View"));*/
 
 	insertWidget(wid);
 	setStyleSheet(StyleSheet::QLabelWhite());
-	show();
+	exec();
 }
 
 void InfoWidget::dataMenu(QKeyEvent*event)
@@ -62,15 +87,22 @@ void InfoWidget::dataMenu(QKeyEvent*event)
 void InfoWidget::configureDataMenu()
 {
 	auto wid = new QWidget;
-	auto lay = new QVBoxLayout;
+	auto lay = new QGridLayout;
 	wid->setLayout(lay);
-	lay->addWidget(new QLabel("Access Key.....I"));
-	lay->addWidget(new QLabel("F ..... Toggle FPS Info"));
-	lay->addWidget(new QLabel("V ..... Toggle View Info"));	
+	lay->addWidget(new QLabel("Any other key to dismiss"), 20, 0, 1, 2);
+	lay->addWidget(new QLabel("Access Key.....I"),0,0,1,2);
+	lay->addWidget(new QLabel("F"), 1, 0, 1, 1);
+	lay->addWidget(new QLabel(".....Toggle FPS Info"), 1, 1, 1, 1); 
+	lay->addWidget(new QLabel("V"), 2, 0, 1, 1);
+	lay->addWidget(new QLabel(".....Toggle View Info"), 2, 1, 1, 1);
+
+
+	/*lay->addWidget(new QLabel("F ..... Toggle FPS Info"));
+	lay->addWidget(new QLabel("V ..... Toggle View Info"));	*/
 
 	insertWidget(wid);
 	setStyleSheet(StyleSheet::QLabelWhite());
-	show();
+	exec();
 }
 
 void InfoWidget::workspaceMenu(QKeyEvent* event)
@@ -85,19 +117,35 @@ void InfoWidget::workspaceMenu(QKeyEvent* event)
 void InfoWidget::configureWorkspaceMenu()
 {
 	auto wid = new QWidget;
-	auto lay = new QVBoxLayout;
+	auto lay = new QGridLayout;
 	wid->setLayout(lay);
-	lay->addWidget(new QLabel("Access Key.....v"));
-	lay->addWidget(new QLabel("S ..... Scene Window"));
-	if (UiManager::isSceneOpen) lay->addWidget(new QLabel("P ..... Player Window"));
-	if (UiManager::isSceneOpen) lay->addWidget(new QLabel("E ..... Editor Window"));
-	lay->addWidget(new QLabel("M ..... Materials Window"));
-	lay->addWidget(new QLabel("A ..... Asset Window"));
+	lay->addWidget(new QLabel("Access Key.....v"), 0, 1,1,2);
+	lay->addWidget(new QLabel("Any other key to dismiss"), 20, 1, 1, 2);
+
+	lay->addWidget(new QLabel("S"), 1, 1, 1, 1);
+	lay->addWidget(new QLabel(".....Scene Window"), 1, 2, 1, 1);
+	if (UiManager::isSceneOpen) {
+		lay->addWidget(new QLabel("P"), 2, 1, 1, 1);
+		lay->addWidget(new QLabel(".....Player Window"), 2, 2, 1, 1);
+		lay->addWidget(new QLabel("E"), 3, 1, 1, 1);
+		lay->addWidget(new QLabel(".....Editor Window"), 3, 2, 1, 1);
+	}
+	lay->addWidget(new QLabel("M"), 4, 1, 1, 1);
+	lay->addWidget(new QLabel(".....Materials Window"), 4, 2, 1, 1);
+	lay->addWidget(new QLabel("A"), 5, 1, 1, 1);
+	lay->addWidget(new QLabel(".....Asset Window"), 5, 2, 1, 1);
+
+
+	//lay->addWidget(new QLabel("S ..... Scene Window"));
+	//if (UiManager::isSceneOpen) lay->addWidget(new QLabel("P ..... Player Window"));
+	//if (UiManager::isSceneOpen) lay->addWidget(new QLabel("E ..... Editor Window"));
+	//lay->addWidget(new QLabel("M ..... Materials Window"));
+	//lay->addWidget(new QLabel("A ..... Asset Window"));
 
 
 	insertWidget(wid);
 	setStyleSheet(StyleSheet::QLabelWhite());
-	show();
+	exec();
 }
 
 void InfoWidget::keyPressEvent(QKeyEvent* event)
