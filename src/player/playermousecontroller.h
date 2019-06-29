0de352bffd8d2a6d39767400e8dc438006e5ad40
Nicolas Brown <nclsbrwn@gmail.com>
2019-06-28 22:27:48 -0500

    Add picking constraint to playback

--- src/player/playermousecontroller.h
+++ src/player/playermousecontroller.h
@@ -16,11 +16,13 @@ For more information see the LICENSE file
 #include <QQuaternion>
 #include <QVector3D>
 #include "../editor/cameracontrollerbase.h"
+#include "../widgets/sceneviewwidget.h"
 
 class PlayerMouseController : public CameraControllerBase
 {
 	iris::ScenePtr scene;
-	//iris::CameraNodePtr camera;
+    //iris::CameraNodePtr camera;
+    iris::SceneNodePtr pickedNode;
 	iris::ViewerNodePtr viewer;
 
     float movementSpeed = 25;
@@ -36,6 +38,8 @@ class PlayerMouseController : public CameraControllerBase
 
 	bool shouldRestoreCameraTransform;
 
+    iris::Viewport viewport;
+
 public:
 	void setPlayState(bool playState) { _isPlaying = playState; }
 
@@ -48,6 +52,18 @@ public:
 	void postUpdate(float dt) override;
     void onMouseMove(int dx,int dy) override;
     void onMouseWheel(int delta) override;
+    void onMouseDown(Qt::MouseButton button) override;
+    void onMouseUp(Qt::MouseButton button) override;
+    QVector3D calculateMouseRay(const QPointF& pos);
+
+    void doObjectPicking(
+        const QPointF& point);
+    QVector3D screenSpaceToWoldSpace(const QPointF& pos, float depth);
+    void doScenePicking(const iris::SceneNodePtr& sceneNode,
+                        const QVector3D& segStart,
+                        const QVector3D& segEnd,
+                        QList<PickingResult>& hitList);
+    void setViewport(const iris::Viewport& viewport);
 
     void updateCameraTransform();
 	void captureYawPitchRollFromCamera();
