1b677cfb0bf4d6dfd90431f35c21dedf4c199ebe
Willroy <willroymyles@yahoo.com>
2019-07-12 05:32:50 -0500

    Used npm package fileicon to set icon for app and dmg

--- cmake/ReleaseProcess.cmake
+++ cmake/ReleaseProcess.cmake
@@ -4,6 +4,15 @@ if(APPLE)
 	get_filename_component(MACDEPLOYQT ${QT5_QMAKE_EXECUTABLE} PATH)
 	set(MACDEPLOYQT "${MACDEPLOYQT}/macdeployqt")
 
+        set(ICON ${CMAKE_CURRENT_SOURCE_DIR}/app/icons/icon.icns)
+
+        set_source_files_properties(${ICON} PROPERTIES MACOSX_PACKAGE_LOCATION "Resources")
+
+        # using fileicon from a npm package to set icon for app
+        # https://github.com/mklement0/fileicon#installation
+        add_custom_command(
+                TARGET ${CMAKE_PROJECT_NAME} POST_BUILD
+                COMMAND /usr/local/bin/fileicon set ${DestDir}/${PROJECT_NAME}.app ${ICON})
 
 	add_custom_command(
 		TARGET ${CMAKE_PROJECT_NAME} POST_BUILD
@@ -29,15 +38,10 @@ if(APPLE)
 		TARGET ${CMAKE_PROJECT_NAME} POST_BUILD
 		COMMAND /usr/sbin/diskutil rename ${DestDir}/${PROJECT_NAME} ${CMAKE_PROJECT_NAME})
 
-	add_custom_command(
-		TARGET ${CMAKE_PROJECT_NAME} POST_BUILD
-                COMMAND fileicon set $rsrc ${BUNDLE_ICON})
+            # using fileicon from a npm package to set icon for dmg
+            # https://github.com/mklement0/fileicon#installation
+            add_custom_command(
+                    TARGET ${CMAKE_PROJECT_NAME} POST_BUILD
+                    COMMAND /usr/local/bin/fileicon set ${DestDir}/${PROJECT_NAME}.dmg ${ICON})
 
-	add_custom_command(
-		TARGET ${CMAKE_PROJECT_NAME} POST_BUILD
-		COMMAND SetFile -a C ${DestDir}/${PROJECT_NAME}.dmg )
-
-	add_custom_command(
-		TARGET ${CMAKE_PROJECT_NAME} POST_BUILD
-		COMMAND Rez -append $rsrc -o ${DestDir}/${PROJECT_NAME}.dmg )
 endif()
