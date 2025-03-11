3f6fbc708f343fa808d83a68cdfeffaebfeb2cdc
Mario Dennis <mdennis876@gmail.com>
2025-03-11 13:41:15 -0500

    update to Qt6

--- cmake/CopyDependencies.cmake
+++ cmake/CopyDependencies.cmake
@@ -1,10 +1,9 @@
 # https://gist.github.com/Rod-Persky/e6b93e9ee31f9516261b
 # This depends on the compiler we are using, see top level file for DestDir
 if (WIN32)
-get_target_property(QT5_QMAKE_EXECUTABLE Qt5::qmake IMPORTED_LOCATION)
-get_filename_component(QT5_WINDEPLOYQT_EXECUTABLE ${QT5_QMAKE_EXECUTABLE} PATH)
-set(QT5_WINDEPLOYQT_EXECUTABLE "${QT5_WINDEPLOYQT_EXECUTABLE}/windeployqt.exe")
+    get_filename_component(QT6_WINDEPLOYQT_EXECUTABLE "${QT_QMAKE_EXECUTABLE}" DIRECTORY) # Use QT_QMAKE_EXECUTABLE
+    set(QT6_WINDEPLOYQT_EXECUTABLE "${QT6_WINDEPLOYQT_EXECUTABLE}/windeployqt.exe")
 
-add_custom_command(TARGET ${CMAKE_PROJECT_NAME} POST_BUILD
-    COMMAND ${QT5_WINDEPLOYQT_EXECUTABLE} $<TARGET_FILE:${CMAKE_PROJECT_NAME}> -multimedia)
+    add_custom_command(TARGET ${CMAKE_PROJECT_NAME} POST_BUILD
+        COMMAND ${QT6_WINDEPLOYQT_EXECUTABLE} $<TARGET_FILE:${CMAKE_PROJECT_NAME}> -multimedia)
 endif(WIN32)
--- cmake/IncludeShaderGraph.cmake
+++ cmake/IncludeShaderGraph.cmake
@@ -7,7 +7,7 @@ set(CMAKE_CXX_STANDARD_REQUIRED True)
 #set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib)
 #set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
 
-find_package(Qt5 REQUIRED COMPONENTS Widgets Core )
+find_package(Qt6 REQUIRED COMPONENTS Widgets Core )
 add_definitions(-DEFFECT_BUILD_AS_LIB )
 
 #add_subdirectory(irisgl)
