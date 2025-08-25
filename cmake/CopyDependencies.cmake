c1df27b445ef98f6e8d15284200043a9c912931e
ronoaer <ronoaer@gmail.com>
2025-08-25 15:50:14 +0800

    update copy-script for linux-debug model

--- cmake/CopyDependencies.cmake
+++ cmake/CopyDependencies.cmake
@@ -22,32 +22,40 @@ if (WIN32)
     )
 
 elseif(UNIX)
-    message("................................... ${CMAKE_BINARY_DIR}   ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}")
-    # Determine Qt6 base directory from Qt6Core_DIR
-    get_filename_component(QT6_BASE_DIR "${Qt6Core_DIR}/../../../" ABSOLUTE)
+    if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+        add_custom_command(
+            OUTPUT "${DEPLOY_STAMP}"
+            COMMAND ${CMAKE_COMMAND} -E touch "${DEPLOY_STAMP}"
+            DEPENDS ${CMAKE_PROJECT_NAME}
+            COMMENT "Nothing..."
+        )
+    else()
+        #Determine Qt6 base directory from Qt6Core_DIR
+        get_filename_component(QT6_BASE_DIR "${Qt6Core_DIR}/../../../" ABSOLUTE)
 
-    add_custom_command(
-        OUTPUT "${DEPLOY_STAMP}"
-        COMMAND ${CMAKE_COMMAND} -E echo "Deploying ${CMAKE_PROJECT_NAME} and dependencies..."
-        COMMAND bash "${CMAKE_SOURCE_DIR}/deploy/data/linux/copy_linux_deps.sh"
-                "$<TARGET_FILE:${CMAKE_PROJECT_NAME}>"          # executable
-                "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}"             # destination
-                "${QT6_BASE_DIR}"                               # Qt6 base
+        add_custom_command(
+            OUTPUT "${DEPLOY_STAMP}"
+            COMMAND ${CMAKE_COMMAND} -E echo "Deploying ${CMAKE_PROJECT_NAME} and dependencies..."
+            COMMAND bash "${CMAKE_SOURCE_DIR}/deploy/data/linux/copy_linux_deps.sh"
+                    "$<TARGET_FILE:${CMAKE_PROJECT_NAME}>"          # executable
+                    "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}"             # destination
+                    "${QT6_BASE_DIR}"                               # Qt6 base
 
 
-        COMMAND ${CMAKE_COMMAND} -E copy
-                # "${CMAKE_BINARY_DIR}/irisgl/libIrisGL.so"
-                # "${CMAKE_BINARY_DIR}/irisgl/src/assimp/bin/libassimp.so.6.0.2"
-                "${CMAKE_BINARY_DIR}/irisgl/src/zip/libzip.so"
-                "${CMAKE_BINARY_DIR}/irisgl/src/bullet3/src/BulletDynamics/libBulletDynamics.so.2.88"
-                "${CMAKE_BINARY_DIR}/irisgl/src/bullet3/src/LinearMath/libLinearMath.so.2.88"
-                "${CMAKE_BINARY_DIR}/irisgl/src/bullet3/src/BulletCollision/libBulletCollision.so.2.88"
-                "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/"
+            COMMAND ${CMAKE_COMMAND} -E copy
+                    # "${CMAKE_BINARY_DIR}/irisgl/libIrisGL.so"
+                    # "${CMAKE_BINARY_DIR}/irisgl/src/assimp/bin/libassimp.so.6.0.2"
+                    "${CMAKE_BINARY_DIR}/irisgl/src/zip/libzip.so"
+                    "${CMAKE_BINARY_DIR}/irisgl/src/bullet3/src/BulletDynamics/libBulletDynamics.so.2.88"
+                    "${CMAKE_BINARY_DIR}/irisgl/src/bullet3/src/LinearMath/libLinearMath.so.2.88"
+                    "${CMAKE_BINARY_DIR}/irisgl/src/bullet3/src/BulletCollision/libBulletCollision.so.2.88"
+                    "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/"
 
-        COMMAND ${CMAKE_COMMAND} -E touch "${DEPLOY_STAMP}"
-        DEPENDS ${CMAKE_PROJECT_NAME}
-        COMMENT "[Deploy] Copying all runtime dependencies after build"
-    )
+            COMMAND ${CMAKE_COMMAND} -E touch "${DEPLOY_STAMP}"
+            DEPENDS ${CMAKE_PROJECT_NAME}
+            COMMENT "[Deploy] Copying all runtime dependencies after build"
+        )
+    endif()
 
 else()
     message(WARNING "Deploy target not defined for this platform")
